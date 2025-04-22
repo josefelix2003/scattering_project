9fe72c9c1a8773f59ef60ef615e782db086799a5
AlexanderKhaldi <alexander.khaldi@etu.umontpellier.fr>
2025-04-22 12:02:18 +0200

    Update entrees.m

--- entrees.m
+++ entrees.m
@@ -1,18 +1,29 @@
-% Demander à l'utilisateur d'entrer les valeurs
+% Demander les valeurs
 Ix = input('Veuillez entrer Ix: ');
 Ix = double(Ix);  % Conversion en type flottant
 
 Iy = input('Veuillez entrer Iy: ');
-Iy = double(Iy);  % Conversion en type flottant
+Iy = double(Iy); 
 
 Rx = input('Veuillez entrer Rx: ');
-Rx = double(Rx);  % Conversion en type flottant
+Rx = double(Rx); 
 
 Ry = input('Veuillez entrer Ry: ');
-Ry = double(Ry);  % Conversion en type flottant
+Ry = double(Ry); 
 
 beta = input('Veuillez entrer beta: ');
-beta = double(beta);  % Conversion en type flottant
+beta = double(beta);  
+
+gamman = zeros(1, 2*M + 1);    
+n_val = -M:M;
+
+for i = 1:(2*M + 1)
+    n = n_val(i);
+    alpha_n = alpha + n * 2 * pi / d;
+    gamma_n = sqrt(k_p^2 - alpha_n^2 - beta^2);
+    gamman(i) = gamma_n;
+end
+
 
 Iz=-(alpha*Ix+beta*Iy)/(gamma1)
 Jz=-(alpha*Jx+beta*Jy)/(gamma2)
