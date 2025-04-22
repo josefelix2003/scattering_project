653bc186e9c399170ef5757be49e6cf6806b893c
AlexanderKhaldi <alexander.khaldi@etu.umontpellier.fr>
2025-04-22 11:00:45 +0200

    Update sigma.m

--- sigma.m
+++ sigma.m
@@ -1,15 +1,19 @@
 e=1,602e-(19)
 hbar=1,05e(-34)
-k_b=1,38e-23
+k_b=1,38e(-23)
 T=300
 mu_c=1
 
+function [sigma_intra]=f(omega,gamma,e,hbar,k_b,T,mu_c)
 
-
+sigma_inter=(2*i*e^2*k_b*T)/(3,14*(hbar^2)*(omega+i*gamma)*ln((2*cosh((mu_c)/(2*k_b*T))))
 function [sigma_inter]=f(omega, gamma,e,hbar,k_b,T,mu_c)
 
-  sigma_inter=e*e/(4*hbar)*(0,5+1/3,14*arctan(((hbar)*(omega+i*gamma)-2*mu_c)/(2*k_b*T))-i/(2*3,14)*ln(((hbar*(omega+i*gamma)+2*mu_c))^2/(((hbar)*(omega+i*gamma)-2*mu^_c)^2)+(2*k_b*T)^2)
+  return
+
+  sigma_inter=e*e/(4*hbar)*(0,5+1/3,14*atan(((hbar)*(omega+i*gamma)-2*mu_c)/(2*k_b*T))-i/(2*3,14)*ln(((hbar*(omega+i*gamma)+2*mu_c))^2/(((hbar)*(omega+i*gamma)-2*mu^_c)^2)+(2*k_b*T)^2)
 
   return
 
  sigma_inter(omega, gamma,e,hbar,k_b,T,mu_c)
+ sigma_intra(omega, gamma,e,hbar,k_b,T,mu_c)
